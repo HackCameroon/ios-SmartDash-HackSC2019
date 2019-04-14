a0a36e5a5a37fab93bcdde1c2b42517154118a5d
Gavin Bains <gbains@usc.edu>
2019-04-13 18:14:37 -0700

    well shit

---
+++ getting-started-ios-sdk/app/getting-started-ios-sdk/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  getting-started-ios-sdk
+//
+//  Created by Smartcar on 11/28/18.
+//  Copyright © 2018 Smartcar. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let clientId = "751d860e-24dc-4777-bb35-ba77a13c27db"
+    static let appServer = "http://localhost:8000"
+}
---
+++ getting-started-ios-sdk/tutorial/getting-started-ios-sdk/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  getting-started-ios-sdk
+//
+//  Created by Smartcar on 11/19/18.
+//  Copyright © 2018 Smartcar. All rights reserved.
+//
+
+import Alamofire
+import UIKit
+import SmartcarAuth
+
+class ViewController: UIViewController {
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var vehicleText = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // TODO: Authorization Step 1: Initialize the Smartcar object
+        
+        // display a button
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 250, height: 50))
+        button.addTarget(self, action: #selector(self.connectPressed(_:)), for: .touchUpInside)
+        button.setTitle("Connect your vehicle", for: .normal)
+        button.backgroundColor = UIColor.black
+        self.view.addSubview(button)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func connectPressed(_ sender: UIButton) {
+        // TODO: Authorization Step 2: Launch authorization flow
+    }
+    
+    func completion(err: Error?, code: String?, state: String?) -> Any {
+        // TODO: Authorization Step 3b: Receive an authorization code
+        
+        // TODO: Request Step 1: Obtain an access token
+        
+        // TODO: Request Step 2: Get vehicle information
+        
+        return ""
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destinationVC = segue.destination as? InfoViewController {
+            destinationVC.text = self.vehicleText
+        }
+    }
+
+}
+
